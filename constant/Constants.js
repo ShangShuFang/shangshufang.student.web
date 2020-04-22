f7abd13dd16575c9b47b117cc4ba1049e58f1a71
zhangqiang <johnny.q.zhang@outlook.com>
2020-04-23 00:01:40 +0800

    学生端框架搭建完成

---
+++ constant/Constants.js
@@ -0,0 +1,10 @@
+exports.ACCOUNT_ROLE = {
+  ADMIN: 'A',
+  TEACHER: 'T',
+  STUDENT: 'S'
+};
+exports.TECHNOLOGY_DEFAULT_ID = 0;
+exports.LEARNING_PHASE = 0;
+exports.PAGE_NUMBER_DEFAULT = 1;
+exports.ACCOUNT_WAITING = 'P';
+exports.DATA_ACTIVE = 'A';
