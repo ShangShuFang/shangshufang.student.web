a0b785634e2b22ee7ac833ef086b1666a72a1050
zhangqiang <johnny.q.zhang@outlook.com>
2020-06-26 12:39:25 +0800

    我的问答开发完成

---
+++ public/javascripts/views/qaCenter.js
@@ -0,0 +1,101 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        questionModel: {
+            pageNumber: 1,
+            maxPageNumber: 0,
+            totalCount: 0,
+            dataList: [],
+        },
+        answerModel: {
+            pageNumber: 1,
+            maxPageNumber: 0,
+            totalCount: 0,
+            dataList: []
+        },
+        dialogModel: {
+            modalTitle: '',
+            questionContent: '',
+            dataList: []
+        },
+        loginUser: commonUtility.getLoginUser()
+    },
+    methods: {
+        initPage: function() {
+            // commonUtility.setNavActive(3);
+            this.loadQuestionList();
+            this.loadAnswerList();
+        },
+        loadQuestionList: function() {
+            let that = this;
+            axios.get(`/center/qa/list/question?pageNumber=${this.questionModel.pageNumber}&studentID=${this.loginUser.studentID}`)
+                .then(res => {
+                    if (res.data.err) {
+                        message.error(localMessage.exception(res.data.code, res.data.msg));
+                        return false;
+                    }
+                    that.questionModel.totalCount = res.data.dataContent.totalCount;
+                    that.questionModel.pageNumber = parseInt(res.data.dataContent.currentPageNum);
+                    that.questionModel.maxPageNumber = Math.ceil(res.data.dataContent.totalCount / res.data.dataContent.pageSize);
+
+                    if (!commonUtility.isEmptyList(res.data.dataContent.dataList)) {
+                        res.data.dataContent.dataList.forEach(function(data) {
+                            that.questionModel.dataList.push(data);
+                        });
+                    }
+                })
+                .catch(err => {
+                    message.error(localMessage.NETWORK_ERROR);
+                });
+        },
+        loadMoreQuestion: function() {
+            this.questionModel.pageNumber++;
+            this.loadQuestionList();
+        },
+        onShowAnswerModal: function(data) {
+            this.dialogModel.modalTitle = `共计${data.leaveMessageCount}条回复`;
+            this.dialogModel.questionContent = data.questionContent;
+            axios.get(`/center/qa/list/question/answer?questionID=${data.questionID}`)
+                .then(res => {
+                    if (res.data.err) {
+                        message.error(localMessage.exception(res.data.code, res.data.msg));
+                        return false;
+                    }
+                    this.dialogModel.dataList = res.data.dataList;
+                })
+                .catch(err => {
+                    message.error(localMessage.NETWORK_ERROR);
+                });
+            $('#modal_answer').modal('show');
+        },
+        loadAnswerList: function() {
+            let that = this;
+            axios.get(`/center/qa/list/answer?pageNumber=${this.answerModel.pageNumber}&studentID=${this.loginUser.studentID}`)
+                .then(res => {
+                    if (res.data.err) {
+                        message.error(localMessage.exception(res.data.code, res.data.msg));
+                        return false;
+                    }
+                    that.answerModel.totalCount = res.data.dataContent.totalCount;
+                    that.answerModel.pageNumber = parseInt(res.data.dataContent.currentPageNum);
+                    that.answerModel.maxPageNumber = Math.ceil(res.data.dataContent.totalCount / res.data.dataContent.pageSize);
+
+                    if (!commonUtility.isEmptyList(res.data.dataContent.dataList)) {
+                        res.data.dataContent.dataList.forEach(function(data) {
+                            that.answerModel.dataList.push(data);
+                        });
+                    }
+                })
+                .catch(err => {
+                    message.error(localMessage.NETWORK_ERROR);
+                });
+        },
+        loadMoreAnswer: function() {
+            this.answerModel.pageNumber++;
+            this.loadAnswerList();
+        },
+    },
+    mounted() {
+        this.initPage();
+    },
+});
--- routes/qaCenter.js
+++ routes/qaCenter.js
@@ -2,10 +2,92 @@ let express = require('express');
 let axios = require('axios');
 let buildUtils = require('../common/buildUtils');
 let customerMessage = require('../config/customerMessage');
+const sysConfig = require('../config/sysConfig.json');
 let router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render('qaCenter', { title: '个人中心' });
+});
+
+router.get('/list/question', function(req, res, next) {
+    const apiKey = 'studentQuestionList';
+    const pageNumber = req.query.pageNumber;
+    const pageSize = sysConfig.pageSize.ten;
+    const studentID = req.query.studentID;
+
+    const parameters = [pageNumber, pageSize, studentID];
+    const requestUri = buildUtils.buildRequestApiUri(apiKey, parameters);
+
+    axios.get(requestUri)
+        .then(result => {
+            let dataContent = buildUtils.buildRenderData(pageNumber, pageSize, result);
+            res.json({
+                err: !result.data.result,
+                code: result.data.responseCode,
+                msg: result.data.responseMessage,
+                dataContent: dataContent
+            });
+        })
+        .catch(error => {
+            res.json({
+                err: true,
+                code: error.code,
+                msg: customerMessage[error.code]
+            });
+        });
+});
+
+router.get('/list/question/answer', function(req, res, next) {
+    const apiKey = 'questionAnswerList';
+    const questionID = req.query.questionID;
+
+    const parameters = [questionID];
+    const requestUri = buildUtils.buildRequestApiUri(apiKey, parameters);
+
+    axios.get(requestUri)
+        .then(result => {
+            res.json({
+                err: !result.data.result,
+                code: result.data.responseCode,
+                msg: result.data.responseMessage,
+                dataList: result.data.responseData,
+            });
+        })
+        .catch(error => {
+            res.json({
+                err: true,
+                code: error.code,
+                msg: customerMessage[error.code]
+            });
+        });
+});
+
+router.get('/list/answer', function(req, res, next) {
+    const apiKey = 'studentAnswerList';
+    const pageNumber = req.query.pageNumber;
+    const pageSize = sysConfig.pageSize.ten;
+    const studentID = req.query.studentID;
+
+    const parameters = [pageNumber, pageSize, studentID];
+    const requestUri = buildUtils.buildRequestApiUri(apiKey, parameters);
+
+    axios.get(requestUri)
+        .then(result => {
+            let dataContent = buildUtils.buildRenderData(pageNumber, pageSize, result);
+            res.json({
+                err: !result.data.result,
+                code: result.data.responseCode,
+                msg: result.data.responseMessage,
+                dataContent: dataContent
+            });
+        })
+        .catch(error => {
+            res.json({
+                err: true,
+                code: error.code,
+                msg: customerMessage[error.code]
+            });
+        });
 });
 
 module.exports = router;
