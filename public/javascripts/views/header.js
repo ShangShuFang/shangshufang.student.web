348254536f8f54d253acb36d5eec4753b76e4507
zhangqiang <johnny.q.zhang@outlook.com>
2020-07-10 14:35:27 +0800

    Fix bug: 固定顶部菜单位置

--- public/javascripts/views/header.js
+++ public/javascripts/views/header.js
@@ -1,48 +1,48 @@
 const vm = new Vue({
-  el: '#kt_header',
-  data: {
-    isLogin: false,
-    userName: '',
-    userPhoto: '',
-    universityName: '',
-    schoolName: '',
-    majorName: ''
-  },
-  methods: {
-    setHeader: function () {
-      this.isLogin = commonUtility.isLogin();
-      const KT_HIDDEN_CLASS = 'kt-hidden';
-      let header_logout = $('.header-logout');
-      let header_login = $('.header-login');
-      if (this.isLogin) {
-        $(header_logout).addClass(KT_HIDDEN_CLASS);
-        $(header_login).removeClass(KT_HIDDEN_CLASS);
-        let userInfo = JSON.parse(commonUtility.getCookie(Constants.COOKIE_LOGIN_USER));
-        this.userName = userInfo.fullName;
-        this.userPhoto = userInfo.photo;
-        if (this.userPhoto.length === 0) {
-          this.userPhoto = '/media/users/user_default.png';
+    el: '#kt_header',
+    data: {
+        isLogin: false,
+        userName: '',
+        userPhoto: '',
+        universityName: '',
+        schoolName: '',
+        majorName: ''
+    },
+    methods: {
+        setHeader: function() {
+            this.isLogin = commonUtility.isLogin();
+            const KT_HIDDEN_CLASS = 'kt-hidden';
+            let header_logout = $('.header-logout');
+            let header_login = $('.header-login');
+            if (this.isLogin) {
+                $(header_logout).addClass(KT_HIDDEN_CLASS);
+                $(header_login).removeClass(KT_HIDDEN_CLASS);
+                let userInfo = JSON.parse(commonUtility.getCookie(Constants.COOKIE_LOGIN_USER));
+                this.userName = userInfo.fullName;
+                this.userPhoto = userInfo.photo;
+                if (this.userPhoto.length === 0) {
+                    this.userPhoto = '/media/users/user_default.png';
+                }
+                this.universityName = userInfo.universityName;
+                this.schoolName = userInfo.schoolName;
+                this.majorName = userInfo.majorName;
+                return false;
+            }
+            $(header_logout).removeClass(KT_HIDDEN_CLASS);
+            $(header_login).addClass(KT_HIDDEN_CLASS);
+        },
+        onLogin: function() {
+            location.href = '/login';
+        },
+        onRegister: function() {
+            location.href = '/register';
+        },
+        onSignOut: function() {
+            commonUtility.delCookie(Constants.COOKIE_LOGIN_USER);
+            location.href = '/';
         }
-        this.universityName = userInfo.universityName;
-        this.schoolName = userInfo.schoolName;
-        this.majorName = userInfo.majorName;
-        return false;
-      }
-      $(header_logout).removeClass(KT_HIDDEN_CLASS);
-      $(header_login).addClass(KT_HIDDEN_CLASS);
     },
-    onLogin: function () {
-      location.href = '/login';
-    },
-    onRegister: function () {
-      location.href = '/register';
-    },
-    onSignOut: function () {
-      commonUtility.delCookie(Constants.COOKIE_LOGIN_USER);
-      location.href = '/';
+    mounted: function() {
+        this.setHeader();
     }
-  },
-  mounted: function () {
-    this.setHeader();
-  }
 });
